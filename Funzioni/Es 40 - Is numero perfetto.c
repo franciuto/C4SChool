09ecdf354eb3f10aceb955c4092d17af36ce8a8d
Francesco Fontanesi <francesco.traversetolo@gmail.com>
2024-12-27 14:53:48 +0100

    Update Es 40 - Is numero perfetto.c

--- Funzioni/Es 40 - Is numero perfetto.c
+++ Funzioni/Es 40 - Is numero perfetto.c
@@ -3,6 +3,8 @@
 int isNumeroPerfetto(int n)
 {
     int i, sum = 0;
+
+    // Find the divisors and add them to the sum
     for (i = 1; i <= n / 2; i++)
     {
         if (n % i == 0)
@@ -10,6 +12,7 @@ int isNumeroPerfetto(int n)
             sum += i;
         }
     }
+    // If it's a perfect number return 1, else return 0
     if (n == sum)
     {
         return 1;
@@ -19,14 +22,18 @@ int isNumeroPerfetto(int n)
 
 int main()
 {
+    // User Input
     int n;
     printf("Is Numero perfetto?\nInserisci un numero: ");
     scanf("%d", &n);
+    // Check for correct input
     while (n <= 0)
     {
         printf("Valore inserito non valido, inserisci un numero maggiore di 0: ");
         scanf("%d", &n);
     }
+    
+    // Print the result
     if (isNumeroPerfetto(n) == 1)
     {
         printf("Il numero è perfetto");
