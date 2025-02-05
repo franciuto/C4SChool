3edb09f227a01463c17ac8158ffa3ef5b08b344f
Franciuto <francesco.traversetolo@gmail.com>
2025-02-05 19:52:23 +0000

    Update to treasure hunt

--- Matrici/tesoro.c
+++ Matrici/tesoro.c
@@ -6,9 +6,11 @@
 
 void gamePrint(char matrix[dimension][dimension]);
 void clearMatrix(char matrix[dimension][dimension]);
+int checkWin(int r_player, int c_player);
 
 int main()
 {
+    int c_player, r_player;
     // Setup
     srand(time(NULL));
     char gameMatrix[dimension][dimension];
@@ -16,17 +18,27 @@ int main()
     // Clear matrix
     clearMatrix(gameMatrix);
 
-    gamePrint(gameMatrix);
+    // Place treasure
+    int r_treasure = rand() % dimension;
+    int c_treasure = rand() % dimension;
 
-    /*
-        // Place treasure
-        int r_treasure = rand() % dimension;
-        int c_treasure = rand() % dimension;
-        gameMatrix[r_treasure][c_treasure] = 'X';
+    do
+    {
+        // Input request
+        printf("Dove si trova il tesoro?");
+        printf("Riga: ");
+        scanf("%d" , &r_player);
+        printf("Colonna: ");
+        scanf("%d" , &c_player);
 
-
-
-    */
+        while (r_player > 8 || r_player < 1 || c_player > 8 || c_player < 1)
+        {
+            
+        }
+        
+    } while (checkWin(r_player , c_player))
+    
+    
 }
 
 void clearMatrix(char matrix[dimension][dimension])
@@ -36,13 +48,20 @@ void clearMatrix(char matrix[dimension][dimension])
     {
         for (j = 0; j < dimension; j++)
         {
-            matrix[i][j] == ' ';
+            matrix[i][j] = ' ';
         }
     }
 }
 
+int checkWin(int r_player, int c_player) {
+
+
+
+}
+
 void gamePrint(char matrix[dimension][dimension])
 {
+    printf("+---+---+---+---+---+---+---+---+\n");
     int i, j;
     for (i = 0; i < dimension; i++)
     {
@@ -55,7 +74,7 @@ void gamePrint(char matrix[dimension][dimension])
             }
             printf("|");
         }
-        printf("\n+---+---+---+---+---+---+---+\n");
+        printf("\n+---+---+---+---+---+---+---+---+\n");
     }
     for (j = 0; j < dimension; j++)
     {
