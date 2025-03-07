84041b11ffc62c275233faf7e33da3aada223789
Franciuto <francesco.traversetolo@gmail.com>
2025-03-07 16:06:49 +0100

    Esercizi per sabato 8

---
+++ Sistemi/04 - dec2bin/dec2bin.s
@@ -0,0 +1,52 @@
+	.file	"dec2bin.c"
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Inserisci numero in decimale: "
+.LC1:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	nop
+.L2:
+	movl	-12(%rbp), %eax
+	testl	%eax, %eax
+	jne	.L2
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L4
+	call	__stack_chk_fail@PLT
+.L4:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 14.2.1 20250207"
+	.section	.note.GNU-stack,"",@progbits
