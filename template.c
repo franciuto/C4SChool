970922534f8eb1911cf8aa25bfbd119c85e63273
franciuto <francesco.traversetolo@gmail.com>
2025-03-16 18:33:27 +0100

    Aggiunta template per programmi in C

---
+++ template.c
@@ -0,0 +1,37 @@
+/************************************************************************
+* FILENAME : -------.c
+*
+* DESCRIPTION :
+*
+*
+* INPUTS :
+*
+*
+* RETURN :
+*
+*
+* USAGE:
+* >>
+* >>
+*
+* AUTHOR:  	 		START DATE :
+*
+* COPYRIGHT:
+*
+* CHANGES :
+*
+* VERSION 	DATE 	WHO 		DETAIL
+* 1.00
+*
+************************************************************************/
+
+#include <stdio.h>  	//
+
+
+int main(int argc, char *argv[]) 		// complete main prototype
+{
+
+
+    printf("\n\n");
+    return 0;                          //main function termination:  successful termination status (0) is returned to the host environment.
+}
