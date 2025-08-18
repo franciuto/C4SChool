8f8b2d554eb185d42cf23134ac132d101725627d
Franciuto <francesco.traversetolo@gmail.com>
2025-08-18 22:38:12 +0200

    Code function order adjustment

--- Sistemi/Compiti estivi/ES_1/01_Save_ips.c
+++ Sistemi/Compiti estivi/ES_1/01_Save_ips.c
@@ -1,28 +1,67 @@
 // Standard library inclusion
-#include <stdio.h> // printf(), scanf(), fgets(), fopen(), fclose()
+#include <stdio.h>  // printf(), scanf(), fgets(), fopen(), fclose()
 #include <stdlib.h> // exit()
 #include <string.h> // strcmp()
 
 // File path stored as a constant so it can be changed
 #define FILEPATH "ip_list.txt"
 
-int ip_conunt = 1;
+// Global counter for IP numbering
+int ip_count = 1;
+
+// --- Functions ---
+void prompt(char *input_ip);
+void check_file(FILE *file_descriptor);
+void file_print();
+
+int main()
+{
+    // Buffer to save ip inputs
+    char input[16];
+
+    // -- OPEN FILE --
+    FILE *file_descriptor = fopen(FILEPATH, "w");
+    // Check if file descriptor has been successfully returned from fopen
+    check_file(file_descriptor);
+
+    // Intro
+    printf("- IPs saver by Francesco Fontanesi -\n");
+    printf("All IPs are saved in \"%s\"\n", FILEPATH);
+    printf("\nInsert IPs (type: \"fine\" to save and exit):\n");
+
+    while (1)
+    {
+        prompt(input);
+        if (strcmp(input, "fine") == 0)
+        {
+            fclose(file_descriptor);
+            file_print();
+            break;
+        }
+        fprintf(file_descriptor, "%s\n", input);
+        ip_count++;
+    }
+
+    return 0;
+}
+
+// --- Function Definitions ---
 
 /*
 Function: prompt
-Description: This function prompts user to insert the ip adress
+Description: This function prompts user to insert the ip address
 Input: pointer to a static buffer to contain the user input (size 16 Bytes)
 Output: Void because the result is stored in the static buffer given as input
 */
 void prompt(char *input_ip)
 {   
-    printf("%d > ", ip_conunt);
-    scanf("%16s", input_ip);
+    printf("%d > ", ip_count);
+    scanf("%15s", input_ip); // limit to 15 chars to avoid overflow
 }
 
 /*
 Function: check_file
-Description: This function checks if the file descriptor passed trought input is valid by comparing it with NULL
+Description: This function checks if the file descriptor passed through input is valid by comparing it with NULL
 Input: FILE Pointer (file descriptor)
 Output: Void, if the file descriptor is not valid, the function prints an error and exits with error code 1 the program
 */
@@ -30,7 +69,7 @@ void check_file(FILE *file_descriptor)
 {
     if (file_descriptor == NULL)
     {
-        printf("Error opening file");
+        printf("Error opening file\n");
         exit(1);
     }
 }
@@ -45,40 +84,14 @@ void file_print()
 {
     FILE *file_descriptor = fopen(FILEPATH, "r");
     check_file(file_descriptor);
-    printf("Content of \"ip_list.txt\"\n");
-    char line[15]; // 15 because an ip is max 15 char long
-    // Using this approach because "getline" is a POSIX only function
+
+    printf("\nContent of \"%s\":\n", FILEPATH);
+
+    char line[16]; // buffer for IP strings
     while (fgets(line, sizeof(line), file_descriptor) != NULL)
     {
         printf("%s", line);
     }
-}
 
-int main()
-{
-    // Buffer to save ip inputs
-    char input[16];
-
-    // -- OPEN FILE --
-    FILE *file_descriptor = fopen(FILEPATH, "w");
-    // Check if file descriptor has been successfully returned from fopen
-    check_file(file_descriptor);
-
-    // Intro
-    printf("- IPs saver by Francesco Fontanesi -");
-    printf("\nAll IPs are saved in \"ip_list.txt\"");
-    printf("\n\nInsert IPs (type: \"fine\" to save and exit):\n");
-
-    while (1)
-    {
-        prompt(input);
-        if (strcmp(input, "fine") == 0)
-        {
-            fclose(file_descriptor);
-            file_print();
-            break;
-        }
-        fprintf(file_descriptor, "%s", input);
-        ip_conunt++;
-    }
+    fclose(file_descriptor);
 }
--- Sistemi/Compiti estivi/ES_2/02_Count_MACs.c
+++ Sistemi/Compiti estivi/ES_2/02_Count_MACs.c
@@ -1,12 +1,39 @@
-#include <stdio.h> // scanf(), printf(), fopen(), fclose(), fgets()
+#include <stdio.h>  // scanf(), printf(), fopen(), fclose(), fgets()
 #include <stdlib.h> // exit()
 
-// File path stored as a constant so it can be changed
+// File path stored as a constant so it can be changed hard coded
 #define FILEPATH "mac_list.txt"
+
+// --- Functions ---
+void check_file(FILE *file_descriptor);
+
+int main() 
+{
+    // - OPEN FILE -
+    FILE *file_descriptor = fopen(FILEPATH, "r");
+    // Check if the file descriptor has been successfully returned by fopen()
+    check_file(file_descriptor);
+
+    int mac_found = 0;
+    char line[19]; // 17 chars for MAC + '\n' + '\0'
+
+    while (fgets(line, sizeof(line), file_descriptor) != NULL)
+    {
+        printf("%s", line);
+        mac_found++;
+    }
+
+    printf("\n%d MAC Addresses found\n", mac_found);
+    fclose(file_descriptor);
+
+    return 0;
+}
+
+// --- Function Definitions ---
 
 /*
 Function: check_file
-Description: This function checks if the file descriptor passed trought input is valid by comparing it with NULL
+Description: This function checks if the file descriptor passed through input is valid by comparing it with NULL
 Input: FILE Pointer (file descriptor)
 Output: Void, if the file descriptor is not valid, the function prints an error and exits with error code 1 the program
 */
@@ -14,24 +41,7 @@ void check_file(FILE *file_descriptor)
 {
     if (file_descriptor == NULL)
     {
-        printf("Error opening file");
+        printf("Error opening file\n");
         exit(1);
     }
-}
-
-int main () {
-    // - OPEN FILE -
-    FILE *file_descriptor = fopen(FILEPATH, "r");
-    // Check if the file descriptor has been successfully returned by fopen()
-    check_file(file_descriptor);
-
-    int mac_found = 0;
-    char line[19]; // 19 because 17 is too small considering the "\n", a single line is then read in multiple calls of fgets resulting in a wrong line count number
-    while (fgets(line, sizeof(line), file_descriptor) != NULL)
-    {
-        printf("%s", line);
-        mac_found++;
-    }
-    printf("\n%d MAC Addresses found", mac_found);
-    fclose(file_descriptor);
 }
